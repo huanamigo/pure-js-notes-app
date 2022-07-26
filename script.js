e40711457ca844fe592e6090c18a36c44f9c55af
huanc0de <arturfurtak5@gmail.com>
2022-07-26 11:34:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+const addBtn = document.querySelector('.add');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+const deleteAllBtn = document.querySelector('.delete-all');
+const deleteNoteBtn = document.getElementsByClassName('delete-note');
+
+const noteArea = document.querySelector('.note-area');
+const notePanel = document.querySelector('.note-panel');
+const category = document.querySelector('#category');
+const textArea = document.querySelector('#text');
+const error = document.querySelector('.error');
+
+let selectedValue;
+
+let noteID = 0;
+
+let cardID = 0;
+let cardIDwork = 0;
+let cardIDshop = 0;
+let cardIDothers = 0;
+
+const checkErrors = () => {
+  if (textArea.value !== '' && category.selectedIndex !== 0) {
+    error.style.visibility = 'hidden';
+  } else {
+    error.style.visibility = 'visible';
+  }
+};
+
+const createNote = () => {
+  const newNote = document.createElement('div');
+  newNote.classList.add('note');
+  newNote.setAttribute('id', noteID);
+  checkCategory(newNote);
+
+  newNote.innerHTML = `
+        <div class="note-header">
+          <h3 class="note-title">${
+            category.options[category.selectedIndex].innerText
+          } #${cardID}</h3>
+          <button class="delete-note" onclick='deleteNote(${noteID})'>
+            <i class="fas fa-times icon"></i>
+          </button>
+        </div>
+        <div class="note-body">
+          ${textArea.value}
+        </div>`;
+
+  noteArea.appendChild(newNote);
+  noteID++;
+};
+
+const hidePanel = () => {
+  notePanel.style.display = 'none';
+  error.style.visibility = 'hidden';
+  textArea.value = '';
+  category.selectedIndex = 0;
+};
+
+const checkCategory = (note) => {
+  switch (category.options[category.selectedIndex].innerText) {
+    case 'Zakupy':
+      note.classList.add('shop');
+      cardIDshop++;
+      cardID = cardIDshop;
+      break;
+    case 'Praca':
+      note.classList.add('work');
+      cardIDwork++;
+      cardID = cardIDwork;
+      break;
+    case 'Inne':
+      note.classList.add('others');
+      cardIDothers++;
+      cardID = cardIDothers;
+      break;
+  }
+};
+
+const deleteNote = (id) => {
+  let noteToDelete = document.getElementById(id);
+  noteArea.removeChild(noteToDelete);
+};
+
+const deleteAllNotes = () => {
+  noteArea.textContent = '';
+};
+
+addBtn.addEventListener('mousedown', () => {
+  notePanel.style.display = 'flex';
+});
+
+cancelBtn.addEventListener('mousedown', () => {
+  hidePanel();
+});
+
+saveBtn.addEventListener('click', () => {
+  checkErrors();
+  if (textArea.value !== '' && category.selectedIndex !== 0) {
+    createNote();
+    hidePanel();
+  }
+});
+
+deleteAllBtn.addEventListener('click', deleteAllNotes);
